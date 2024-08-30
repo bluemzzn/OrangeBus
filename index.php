c047029bd9287991b46a8cd6ca962bb6f9509c4e
bluemzzn <synthiawithsyren@gmail.com>
2024-08-30 07:45:34 +0700

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,7 +17,7 @@
 				</div>
 
 				<ul id="main">
-					<li><a href="index.html">Home</a></li>
+					<li><a href="index.php">Home</a></li>
 					<li><a href="aboutus.html">About us</a></li>
 					<li><a href="cart.html">Cart</a></li>
 					<li id="login"><a href="login.html">Login/Sign up</a></li>
@@ -61,77 +62,47 @@
 	<section class="product">
 		<div class="container">
 			<h2>Best Options</h2>
-		
-		<div class="allpro">
-		<div class="pro-con">
-			<div class="pro1">
-				<img src="bus1.jpg" width="150px" height="180px">
-				<div class="detail">
-					<span>Date : 1 Sep 2024</span><br>
-					<span>Time: 00:00 - 07:00 am</span><br>
-					<span>By: Orange bus</span><br>
-					<span>Num: AW4526</span><br>
-					<button type="button"  value="click me" class="book-btn">Book now</button>
-				</div>
-			</div>
-			
-		</div>
-		<div class="pro-con">
-			<div class="pro1">
-				<img src="bus1.jpg" width="150px" height="180px">
-				<div class="detail">
-					<span>Date : 1 Sep 2024</span><br>
-					<span>Time: 00:00 - 07:00 am</span><br>
-					<span>By: Orange bus</span><br>
-					<span>Num: AW4526</span><br>
-					<button type="button"  value="click me" class="book-btn">Book now</button>
-				</div>
-			</div>
-			
-		</div>
-		<div class="pro-con">
-			<div class="pro1">
-				<img src="bus1.jpg" width="150px" height="180px">
-				<div class="detail">
-					<span>Date : 1 Sep 2024</span><br>
-					<span>Time: 00:00 - 07:00 am</span><br>
-					<span>By: Orange bus</span><br>
-					<span>Num: AW4526</span><br>
-					<button type="button"  value="click me" class="book-btn">Book now</button>
-				</div>
-			</div>
-			
-		</div>
-		<div class="pro-con">
-			<div class="pro1">
-				<img src="bus1.jpg" width="150px" height="180px">
-				<div class="detail">
-					<span>Date : 1 Sep 2024</span><br>
-					<span>Time: 00:00 - 07:00 am</span><br>
-					<span>By: Orange bus</span><br>
-					<span>Num: AW4526</span><br>
-					<button type="button"  value="click me" class="book-btn">Book now</button>
-				</div>
-			</div>
-			
-		</div>
-		<div class="pro-con">
-			<div class="pro1">
-				<img src="bus1.jpg" width="150px" height="180px">
-				<div class="detail">
-					<span>Date : 1 Sep 2024</span><br>
-					<span>Time: 00:00 - 07:00 am</span><br>
-					<span>By: Orange bus</span><br>
-					<span>Num: AW4526</span><br>
-					<button type="button"  value="click me" class="book-btn">Book now</button>
-				</div>
-			</div>
-			
-		</div>
-		</div>
-		</div>
-	</section>
+		<?php
+			$a = 0;
+			//include("conn.php");
+			$sql = "SELECT * FROM user";
+			//$result = mysqli_query($conn, $sql);
+			echo "<table >";
+			//while ($row = mysqli_fetch_assoc($result)) {
+				for ($i=0; $i < 17; $i++) { 
+					// code...
+					$a = $a + 1;
 
+			//echo "<form method=get action=>";
+				if ($a%2 == 1) {
+					echo "<tr><td>";
+				}else{
+					echo "<td>";
+				}
+				//echo $a;
+				
+				/*
+				foreach ($row as $key => $value) {
+					echo $i%3;
+					echo "$key"."--"."$value<br>";
+					//echo $key."--".$value."<br>";
+					
+				}
+				*/
+				
+					echo "<div class=pro-con><div class=pro1><img src=bus1.jpg width=300px height=180px><div class=detail><span>Date : 1 Sep 2024</span><br><span>Time: 00:00 - 07:00 am</span><br><span>By: Orange bus</span><br><span>Num: AW4526</span><br><button type=button  value=click me class=book-btn>Book now</button></div></div></div>";
+					
+				
+				if ($i%2== 1) {
+					echo "</td></tr>";
+				}else{
+					echo "</td>";
+				}
+			}
+		?>
+
+	</div>
+	</section>
 
 	<footer class="sec1">
 		<div class="foot">
@@ -147,7 +118,6 @@
         <div class="copyright">
             <p>Copyright © MyCompany. All rights reserved.</p>
         </div>
-
 	</footer>
 </body>
 </html>
